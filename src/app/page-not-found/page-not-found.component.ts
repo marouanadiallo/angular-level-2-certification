90ddcc33c7337b9a9817800397364ab44b811f4a
marouana.diallo <marouanadiallo1@gmail.com>
2024-04-07 18:33:37 +0200

    Page not found implemented

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
