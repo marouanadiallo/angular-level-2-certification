d7b23aeae6e761169179d4c26994730f6d884f1c
marouana.diallo <marouanadiallo1@gmail.com>
2024-04-07 23:34:06 +0200

    fixed duration pipe

--- src/app/shared/pipe/format-duration.pipe.ts
+++ src/app/shared/pipe/format-duration.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from "@angular/core";
     standalone: true
 })
 export class FormatDurationPipe implements PipeTransform {
-    transform(duration: number, ...args: string[]): string {
-        const hour = duration/60;
+    transform(duration: number | string, ...args: string[]): string {
+        if(isNaN(+duration)) throw new Error("Duration has to be number");
+        const hour = +duration/60;
         const fr = (hour * 10) % 10 / 10;
         const int = hour - fr;
         return Math.round(int) + "h " + Math.round((fr*60)) +"min";
